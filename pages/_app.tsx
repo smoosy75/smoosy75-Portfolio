29a5740067643aabe5e5baa15595da100078b037
Mustapha Said <mustaphasaid@mustaphas-macbook-air.home>
2023-04-07 11:13:03 +0200

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,14 @@
 import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
+}
 
 export default MyApp;
